bf772d03d244fb8df93996852306d0d784018353
Alonso Salcido <alonsosalcido13@gmail.com>
2023-04-04 16:34:12 -0600

    SHIP using getters on class

--- src/bases/03-clases.ts
+++ src/bases/03-clases.ts
@@ -1,14 +1,18 @@
 export class Pokemon {
+
+    get imageUrl(): string {
+        return `https://pokemon.com/${this.id}.png`;
+    }
+
     constructor( 
         public readonly id: number, 
         public name: string,
-        public imageUrl: string,
     ){}
 }
 
-export const charmander = new Pokemon(4, 'Charmander', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png');
+export const charmander = new Pokemon(4, 'Charmander');
 
 // charmander.id = 10;
 // charmander.name = 'Pikachu';
 
-console.log(charmander);
+console.log(charmander.imageUrl);
