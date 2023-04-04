546850725bacc094a83d1af18ff50d642e02ac47
Alonso Salcido <alonsosalcido13@gmail.com>
2023-04-04 16:48:00 -0600

    SHIP using access modifiers on class

--- src/bases/03-clases.ts
+++ src/bases/03-clases.ts
@@ -8,6 +8,14 @@ export class Pokemon {
         public readonly id: number, 
         public name: string,
     ){}
+
+    scream() {
+        console.log(`I'm ${this.name.toUpperCase()} !!!`);
+    }
+
+    private speak() {
+        console.log(`${this.name} said hello`);
+    }
 }
 
 export const charmander = new Pokemon(4, 'Charmander');
@@ -16,3 +24,5 @@ export const charmander = new Pokemon(4, 'Charmander');
 // charmander.name = 'Pikachu';
 
 console.log(charmander.imageUrl);
+charmander.scream();
+charmander.speak();
