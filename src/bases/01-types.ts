c80ed7e2ffffbd6a1f922b988f9d1ae66ec10263
Alonso Salcido <alonsosalcido13@gmail.com>
2023-04-04 13:16:25 -0600

    SHIP using let, const, and template string

--- src/bases/01-types.ts
+++ src/bases/01-types.ts
@@ -1,3 +1,18 @@
-export const name: string = 'Alonso';
+export let name: string = 'Alonso';
 export const age: number = 35;
 export const isValid: boolean = true;
+
+//Type Safety 
+
+name = 'Melissa';
+
+export const templateString = ` Esto es un string
+multilinea 
+que puede tener 
+"" dobles
+' simples
+inyectar valores ${ name }
+expresiones ${ 1 + 1 }
+numeros: ${ age }
+booleanos: ${ isValid }`;
+console.log("🚀 ~ file: 01-types.ts:18 ~ templateString:", templateString)
